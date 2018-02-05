bca19e862943fc2ba577d64c0d11308f24f0f6bf
zqhuang <zqhuang@brandeis.edu>
2018-02-05 16:18:13 -0500

    created ZiqingCircle.java

---
+++ ZiqingCircle.java
@@ -0,0 +1,12 @@
+package pa03;
+
+public class ZiqingCircle extends CircleShape{
+
+  public static void main(String[] args){
+    ZiqingCircle x= new ZiqingCircle();
+    System.out.println("my circle is "+x);
+  }
+  public String toString(){
+    return "ZiqingCircle:" + super.toString();
+  }
+}
