06e553db2f9a617513faedb3edc43c94c62aa1d4
zqhuang <zqhuang@brandeis.edu>
2018-02-06 13:16:34 -0500

    ZiqingCircle.java

--- ZiqingCircle.java
+++ ZiqingCircle.java
@@ -1,11 +1,19 @@
 package pa03;
 
+import java.awt.Color;
+
 public class ZiqingCircle extends CircleShape{
+
+  public ZiqingCircle(){
+    super();
+    this.color = new Color(0,0,255);
+  }
 
   public static void main(String[] args){
     ZiqingCircle x= new ZiqingCircle();
     System.out.println("my circle is "+x);
   }
+  
   public String toString(){
     return "ZiqingCircle:" + super.toString();
   }
