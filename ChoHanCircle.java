64acc4f4905fde4108cc4b311d088256d2dd00bc
amylauch <chlau@brandeis.edu>
2018-02-08 18:04:30 -0500

    Update ChoHanCircle.java

--- ChoHanCircle.java
+++ ChoHanCircle.java
@@ -1,18 +1,34 @@
 package pa03;
 
 import java.awt.Color;
+import java.awt.Graphics;
 
 public class ChoHanCircle extends CircleShape{
+  private Color borderColor;
 
   public ChoHanCircle(){
     super();
-    this.color = new Color(0,0,0);
+    this.color = new Color(128,128,128);
+    this.borderColor = new Color(0,191,255);
   }
 
+  public void draw(Graphics g){
+    int u = (int)(x-radius);
+    int v = (int)(y-radius);
+    int w = (int)(2*radius);
+    int h = w;
+    g.setColor( color );
+    g.fillOval( u,v,w,h );
+    g.setColor( Color.RED );
+    g.drawOval(u,v,w,h );
+	}
+
+/**
   public static void main(String[] args){
     ChoHanCircle x = new ChoHanCircle();
     System.out.println("my circle is "+x);
   }
+*/
 
   public String toString(){
     return "ChoHanCircle:" + super.toString();
