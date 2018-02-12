bca30a930c17908cb7ab512db941ac2d4d70286c
zqhuang <zqhuang@brandeis.edu>
2018-02-12 09:55:58 -0500

    Ziqing

--- DrawingModel.java
+++ DrawingModel.java
@@ -27,7 +27,7 @@ public class DrawingModel {
 	*/
 	private void init(){
 		CircleShape c;
-		for(int i=0; i< circles.length/3; i++){
+		for(int i=0; i< circles.length/4; i++){
 			// we go to circles.length/2 as we add 2 different kinds of circles
 			// each time through this loop.. with 5 classes you'll divide by 5
 			// WHEN YOU ADD ANOTHER SUBCLASS OF CIRCLESHAPE YOU NEED TO
@@ -40,6 +40,9 @@ public class DrawingModel {
 
       c = new ChoHanCircle();
       this.add(c);
+
+			c = new ZiqingCircle();
+			this.add(c);
 
 		}
 	}
--- ZiqingCircle.java
+++ ZiqingCircle.java
@@ -1,19 +1,30 @@
 package pa03;
 
 import java.awt.Color;
+import java.awt.Graphics;
+
 
 public class ZiqingCircle extends CircleShape{
+  private int random = (int)(Math.random()*255);
 
   public ZiqingCircle(){
     super();
-    this.color = new Color(0,0,255);
+    this.color = new Color(random%256,0,70);
   }
 
-  public static void main(String[] args){
-    ZiqingCircle x= new ZiqingCircle();
-    System.out.println("my circle is "+x);
+  public void draw(Graphics g){
+    int u = (int)(x-radius);
+    int v = (int)(y-radius);
+    int w = (int)(2*radius);
+    int h = w;
+    g.setColor( color );
+    g.fillOval( u,v,w,h );
+    g.setColor( Color.GREEN );
+    g.drawOval(u,v,w,h );
   }
-  
+
+
+
   public String toString(){
     return "ZiqingCircle:" + super.toString();
   }
