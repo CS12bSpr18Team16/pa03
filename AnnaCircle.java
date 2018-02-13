cdc0c7886a0baa3768386064db9a28daa5b57c15
annaJYQ <36045849+annaJYQ@users.noreply.github.com>
2018-02-12 20:21:21 -0500

    Add files via upload

---
+++ AnnaCircle.java
@@ -0,0 +1,29 @@
+package pa03;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+
+public class AnnaCircle extends CircleShape{
+    private double myR= Math.random();
+
+
+    public AnnaCircle(){
+      super();
+      this.color=new Color(255,182,193,100);
+    }
+    public void update(double dt){
+
+  		this.radius += dt*myR;
+
+      if  (this.radius <5){
+        this.myR *= -1;
+      }else if (this.radius > 60){
+        this.myR *= 0;
+      }
+  		super.update(dt);
+    }
+    public String toString(){
+      return "AnnaCircle: "+super.toString();
+    }
+}
