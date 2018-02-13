c7f65eb8a26fe0e81bb98916f2d2fe5349bb5ff4
annaJYQ <36045849+annaJYQ@users.noreply.github.com>
2018-02-12 20:23:24 -0500

    Update AnnaCircle.java

--- AnnaCircle.java
+++ AnnaCircle.java
@@ -7,21 +7,19 @@ import java.awt.Graphics;
 public class AnnaCircle extends CircleShape{
     private double myR= Math.random();
 
-
     public AnnaCircle(){
       super();
       this.color=new Color(255,182,193,100);
     }
     public void update(double dt){
-
-  		this.radius += dt*myR;
+  	  this.radius += dt*myR;
 
       if  (this.radius <5){
         this.myR *= -1;
       }else if (this.radius > 60){
         this.myR *= 0;
       }
-  		super.update(dt);
+      super.update(dt);
     }
     public String toString(){
       return "AnnaCircle: "+super.toString();
