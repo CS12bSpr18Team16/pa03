5eafabaca8aa72ca8f427dcd11fc60a51e6ba4f8
annaJYQ <36045849+annaJYQ@users.noreply.github.com>
2018-02-12 20:30:16 -0500

    Update AnnaCircle.java

--- AnnaCircle.java
+++ AnnaCircle.java
@@ -5,18 +5,23 @@ import java.awt.Graphics;
 
 
 public class AnnaCircle extends CircleShape{
+    //My new instance variable
     private double myR= Math.random();
-
+    //Overrides the default constructor
     public AnnaCircle(){
       super();
-      this.color=new Color(255,182,193,100);
+      this.color=new Color(255,182,193,100);    //transparent light pink
     }
+    
+    //Overrides the update method
+    //Radius increase when radius is smaller than 5
+    //When it is larger 60, it will remain the same
     public void update(double dt){
   	  this.radius += dt*myR;
 
       if  (this.radius <5){
         this.myR *= -1;
-      }else if (this.radius > 60){
+      }else if (this.radius >60){
         this.myR *= 0;
       }
       super.update(dt);
